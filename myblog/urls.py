11cf4cdda3685271fd5ae4d940e3dbcdb39ba21a
unknown <cchisomfrancis@gmail.com>
2022-05-30 13:30:05 +0100

    change debug

--- myblog/urls.py
+++ myblog/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blogapp/', include('blogapp.urls', namespace='blog')),
+    path('', include('blogapp.urls', namespace='blog')),
 ]
