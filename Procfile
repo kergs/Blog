7770390bacb5fb2c03bf9ea9bf5346d066c8c6f4
unknown <cchisomfrancis@gmail.com>
2022-05-30 12:27:07 +0100

    add seetings

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn -b "0.0.0.0:$PORT" -w 3 myblog.wsgi
+release: python manage.py migrate
--- myblog/settings.py
+++ myblog/settings.py
@@ -24,12 +24,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('BLOG_KEY')
+environment = os.getenv('ENVIRONMENT', 'development')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+if environment == 'development':
 
+    DEBUG = True
+    ALLOWED_HOSTS = []
+
+elif environment == "production":
+     DEBUG = False
+     ALLOWED_HOSTS = ['kergs-blog.herokuapp.com']
 
 # Application definition
 
@@ -39,12 +45,15 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
-    'blogapp.apps.BlogappConfig', 'taggit',
+    'blogapp.apps.BlogappConfig', 
+    'taggit',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
